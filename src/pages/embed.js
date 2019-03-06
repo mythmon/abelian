bc1481e8a67d25f6b2d8eb466cc1bcd2a05807da
Mike Cooper <mythmon@gmail.com>
2019-03-05 22:03:31 -0800

    Don't generate next/prev links during server-side rendering

--- src/pages/embed.js
+++ src/pages/embed.js
@@ -3,37 +3,54 @@ import { graphql } from "gatsby";
 
 import styles from "./embed.module.css";
 
-export default function Embed({ data, location }) {
-  const members = data.allMembersYaml.edges.map(e => e.node);
-  let parentIndex = -1;
+export default class Embed extends React.Component {
+  state = {
+    ssr: true,
+  };
 
-  if (parentIndex === -1 && location && location.href) {
-    const parsedLocation = new URL(location.href);
-    const parentId = parsedLocation.searchParams.get("parentId");
-    if (parentId) {
-      parentIndex = members.findIndex(m => m.id === parentId);
+  componentDidMount() {
+    // Client has re-hydrated on the client, trigger a re-render
+    this.setState({ ssr: false });
+  }
+
+  render() {
+    const { data, location } = this.props;
+    const { ssr } = this.state;
+
+    let next, previous;
+    // Don't try and determine a parent during server-side rendering
+    if (!ssr) {
+      const members = data.allMembersYaml.edges.map(e => e.node);
+      let parentIndex = -1;
+
+      // Try and get a parent id from the query string
+      if (parentIndex === -1) {
+        const parsedLocation = new URL(location.href);
+        const parentId = parsedLocation.searchParams.get("parentId");
+        if (parentId) {
+          parentIndex = members.findIndex(m => m.id === parentId);
+        }
+      }
+
+      // Check for an iframe parent
+      if (parentIndex === -1 && typeof window !== "undefined" && window.self !== window.top) {
+        // in an iframe
+        const parentUrl = document.referrer;
+        parentIndex = members.findIndex(m => parentUrl.includes(m.url));
+      }
+
+      if (parentIndex !== -1) {
+        next = members[(parentIndex + 1) % members.length];
+        // negative modulus is weird, so wrap the other way
+        previous = members[(parentIndex + members.length - 1) % members.length];
+      } else {
+        console.log("Couldn't detect webring parent, defaulting to first and last");
+        next = members[0];
+        previous = members[members.length - 1];
+      }
     }
-  }
 
-  if (parentIndex === -1 && typeof window !== "undefined" && window.self !== window.top) {
-    // in an iframe
-    const parentUrl = document.referrer;
-    parentIndex = members.findIndex(m => parentUrl.includes(m.url));
-  }
-
-  let next, previous;
-  if (parentIndex !== -1) {
-    next = members[(parentIndex + 1) % members.length];
-    // negative modulus is weird, so wrap the other way
-    previous = members[(parentIndex + members.length - 1) % members.length];
-  } else {
-    console.log("Couldn't detect webring parent, defaulting to first and last");
-    next = members[0];
-    previous = members[members.length - 1];
-  }
-
-  return (
-    <>
+    return (
       <div className={styles.embedLayout}>
         {previous && (
           <div>
@@ -59,8 +76,8 @@ export default function Embed({ data, location }) {
           </div>
         )}
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export const pageQuery = graphql`
